6585ad5480d121d4276cbf8887a5717349afe68f
ola9292 <okaz692@gmail.com>
2020-08-26 14:14:22 +0100

    clock commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function showTime(){
+    var time = document.querySelector('#time');
+var d = new Date();
+var h = d.getHours();
+var m = d.getMinutes();
+var s = d.getSeconds();
+var session = "AM";
+
+if(h>12){
+    session = "PM";
+}
+
+time.innerHTML =  h + ":" + m + ":" + s + " " + session; 
+setTimeout(showTime, 1000);
+}
+showTime();
